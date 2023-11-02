8ffd103a1c00cbdfe60c2cbe24762fa5c52951a5
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-02 19:07:10 -0400

    @Qualifier @Primary

--- Spring-04-Abstraction/src/main/java/com/service/CommentService.java
+++ Spring-04-Abstraction/src/main/java/com/service/CommentService.java
@@ -3,6 +3,7 @@ package com.service;
 import com.model.Comment;
 import com.proxy.CommentNotificationProxy;
 import com.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,7 +14,7 @@ public class CommentService {
     private final CommentNotificationProxy commentNotificationProxy;
 
 //    @Autowired
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("emailNotification") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
