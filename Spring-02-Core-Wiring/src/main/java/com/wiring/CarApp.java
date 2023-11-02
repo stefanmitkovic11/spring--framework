dea3c9c9c37724a5fcd4d34a0a7cca869f4246e3
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-02 10:27:09 -0400

    Spring-Core-Wiring

---
+++ Spring-02-Core-Wiring/src/main/java/com/wiring/CarApp.java
@@ -0,0 +1,20 @@
+package com.wiring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CarApp {
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigCar.class);
+
+        Car c = context.getBean(Car.class);
+        System.out.println("Car make: "+c.getMake());
+
+        Person p = context.getBean(Person.class);
+        System.out.println("Person name: "+p.getName());
+
+        System.out.println("Person car: "+ p.getCar());
+
+    }
+}
