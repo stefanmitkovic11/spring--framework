2fc0b1e5a7ff7d7f5c6420b1a702fde5eb0dd1ab
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-02 11:37:53 -0400

    Dependency Injection

--- Spring-03-Core-DI/src/main/java/com/Java.java
+++ Spring-03-Core-DI/src/main/java/com/Java.java
@@ -1,13 +1,28 @@
 package com;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class Java {
 
-    @Autowired
+//    ************  Field Injection  **************
+//    @Autowired
+//    OfficeHours officeHours;
+
+
+//    ***********   Constructor Injection ***********
+//    OfficeHours officeHours;
+//
+//    @Autowired
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+
     OfficeHours officeHours;
 
     public void getTeachingHours() {
