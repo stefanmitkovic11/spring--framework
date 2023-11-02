2115a7612a40c9bc77d75312dc464059b8e2600a
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-02 18:02:15 -0400

    Spring-04-Abstraction

---
+++ Spring-04-Abstraction/src/main/java/com/proxy/EmailCommentNotificationProxy.java
@@ -0,0 +1,10 @@
+package com.proxy;
+
+import com.model.Comment;
+
+public class EmailCommentNotificationProxy implements CommentNotificationProxy {
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending notification for comment: "+ comment.getText());
+    }
+}
---
+++ Spring-04-Abstraction/src/main/java/com/repository/DBCommentRepository.java
@@ -0,0 +1,11 @@
+package com.repository;
+
+
+import com.model.Comment;
+
+public class DBCommentRepository implements CommentRepository {
+    @Override
+    public void storeComment(Comment comment) {
+        System.out.println("Storing comment: "+comment.getText());
+    }
+}
