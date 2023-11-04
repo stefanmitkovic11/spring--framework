45733acd0967a6a22ab96f8f6a68cc019cd62fa4
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-04 06:48:57 -0400

    Singleton vs Prototype

--- Spring-05-Core-BeanScope/src/main/java/TestApp.java
+++ Spring-05-Core-BeanScope/src/main/java/TestApp.java
@@ -1,5 +1,35 @@
+import com.config.ProjectConfig;
+import com.model.Comment;
+import com.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class TestApp {
     public static void main(String[] args) {
+        Comment comment = new Comment();
+        comment.setAuthor("Stefan");
+        comment.setText("Hello Spring");
+
+
+        Comment comment2 = new Comment();
+        comment2.setAuthor("Johnson");
+        comment2.setText("Hello");
+
+
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        CommentService cs1 = context.getBean(CommentService.class);
+        CommentService cs2 = context.getBean(CommentService.class);
+
+
+        System.out.println(cs1);
+        System.out.println(cs2);
+
+//      Adding a @Scope("prototype") or @Scope(BeanDefinition.SCOPE_PROTOTYPE) in service class spring will create different objects and will show as false
+        System.out.println(cs1 == cs2);
+
+
 
     }
 }
--- Spring-05-Core-BeanScope/src/main/java/com/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/service/CommentService.java
@@ -4,9 +4,13 @@ import com.model.Comment;
 import com.proxy.CommentNotificationProxy;
 import com.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
 
     private final CommentRepository commentRepository;
