f8915cd744c0dc9ff0d70401bade70da4ef5f6ca
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-04 07:08:29 -0400

    Eager and Lazy Instantiation

--- Spring-05-Core-BeanScope/src/main/java/TestApp.java
+++ Spring-05-Core-BeanScope/src/main/java/TestApp.java
@@ -17,17 +17,19 @@ public class TestApp {
 
 
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+//        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+//
+//        CommentService cs1 = context.getBean(CommentService.class);
+//        CommentService cs2 = context.getBean(CommentService.class);
+//
+//
+//        System.out.println(cs1);
+//        System.out.println(cs2);
 
-        CommentService cs1 = context.getBean(CommentService.class);
-        CommentService cs2 = context.getBean(CommentService.class);
 
-
-        System.out.println(cs1);
-        System.out.println(cs2);
 
 //      Adding a @Scope("prototype") or @Scope(BeanDefinition.SCOPE_PROTOTYPE) in service class spring will create different objects and will show as false
-        System.out.println(cs1 == cs2);
+//        System.out.println(cs1 == cs2);
 
 
 
