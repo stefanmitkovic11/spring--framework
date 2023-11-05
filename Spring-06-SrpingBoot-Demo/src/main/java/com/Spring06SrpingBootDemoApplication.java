c4e6affec1c9cd1d235e9b58f60317402399ff03
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-05 10:52:47 -0500

    Spring-06-SrpingBoot-Demo

---
+++ Spring-06-SrpingBoot-Demo/src/main/java/com/Spring06SrpingBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06SrpingBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring06SrpingBootDemoApplication.class, args);
+	}
+
+}
