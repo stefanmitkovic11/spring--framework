24d13747ada52f1d351427eeaee821bac9d6f2e9
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-05 15:36:57 -0500

    Spring-06-SpringBoot-Demo

--- Spring-06-SpringBoot-Demo/src/main/java/com/Spring06SpringBootDemoApplication.java
+++ Spring-06-SpringBoot-Demo/src/main/java/com/Spring06SpringBootDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring06SrpingBootDemoApplication {
+public class Spring06SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring06SrpingBootDemoApplication.class, args);
+		SpringApplication.run(Spring06SpringBootDemoApplication.class, args);
 	}
 
 }
