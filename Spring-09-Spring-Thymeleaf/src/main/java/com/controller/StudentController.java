98ada2d540018f7d20cb05a9dde6473cb4fc77c4
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-10 07:47:34 -0500

    Spring-09-Thymeleaf

--- Spring-09-Spring-Thymeleaf/src/main/java/com/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/controller/StudentController.java
@@ -3,7 +3,9 @@ package com.controller;
 import com.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.xml.crypto.Data;
 
@@ -20,8 +22,9 @@ public class StudentController {
     }
 
     @RequestMapping("/welcome")
-    public String welcome(Model model){
+    public String welcome(@RequestParam String name, Model model){
 
+        System.out.println(name);
 
         return "student/welcome";
     }
