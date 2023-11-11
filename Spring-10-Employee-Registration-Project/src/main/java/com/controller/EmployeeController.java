14b41fe494ea264fc7881275e05e43baea7688b2
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-11-11 12:37:51 -0500

    Add Validation

--- Spring-10-Employee-Registration-Project/src/main/java/com/controller/EmployeeController.java
+++ Spring-10-Employee-Registration-Project/src/main/java/com/controller/EmployeeController.java
@@ -4,10 +4,13 @@ import com.bootstrap.DataGenerator;
 import com.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -24,7 +27,11 @@ public class EmployeeController {
     }
 
     @PostMapping("/list")
-    public String employeeList(@ModelAttribute("employee") Employee employee, Model model) {
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult,Model model) {
+
+        if(bindingResult.hasErrors()){
+            return "employee/create";
+        }
 
         DataGenerator.addEmployee(employee);
 
