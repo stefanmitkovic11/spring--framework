1c358ffd93f255dc634706134d831f678477014d
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-12-19 09:55:17 -0500

    Spring-16-REST

---
+++ Spring-16-REST/src/main/java/company/Spring16RestApplication.java
@@ -0,0 +1,13 @@
+package company.spring16rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring16RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring16RestApplication.class, args);
+    }
+
+}
---
+++ Spring-16-REST/src/test/java/company/spring16rest/Spring16RestApplicationTests.java
@@ -0,0 +1,13 @@
+package company.spring16rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring16RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
