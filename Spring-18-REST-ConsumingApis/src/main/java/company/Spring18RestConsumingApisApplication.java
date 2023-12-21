bea64a5baba14d3aaff0f1bb554c74eba2a91c95
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-12-21 11:53:37 -0500

    Spring-18-REST-ConsumingApis

---
+++ Spring-18-REST-ConsumingApis/src/main/java/company/Spring18RestConsumingApisApplication.java
@@ -0,0 +1,13 @@
+package company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring18RestConsumingApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring18RestConsumingApisApplication.class, args);
+    }
+
+}
---
+++ Spring-18-REST-ConsumingApis/src/test/java/company/spring18restconsumingapis/Spring18RestConsumingApisApplicationTests.java
@@ -0,0 +1,13 @@
+package company.spring18restconsumingapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring18RestConsumingApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
