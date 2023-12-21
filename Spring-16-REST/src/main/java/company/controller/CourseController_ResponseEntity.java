0e5eaddd03450cc335f8a791be316870ca54c2f4
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-12-21 04:05:07 -0500

    Spring-16-REST ResponseEntity and ResponseWrapper

---
+++ Spring-16-REST/src/main/java/company/controller/CourseController_ResponseEntity.java
@@ -0,0 +1,59 @@
+package company.controller;
+
+import company.dto.CourseDTO;
+import company.entity.Course;
+import company.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/courses/api/v2")
+public class CourseController_ResponseEntity {
+
+    private final CourseService courseService;
+
+    public CourseController_ResponseEntity(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseDTO>> getAllCourses() {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Version","Company.V2")
+                .header("Operation", "Get List")
+                .body(courseService.getCourses());
+
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") Long courseId) {
+
+//   return ResponseEntity.ok(courseService.getCourseById(courseId));
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("Version","Company.V2")
+                .header("Operation","Get Course")
+                .body(courseService.getCourseById(courseId));
+    }
+
+
+    @PostMapping()
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO courseDTO){
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version","Company.V2")
+                .header("Operation","Get Course")
+                .body(courseService.createCourse(courseDTO));
+    }
+
+
+
+
+
+}
