3d597b696a289ec46bb9baff1e035c3d73fa1264
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-12-21 12:17:52 -0500

    Spring-18-REST-ConsumingApis  REST Template

--- Spring-18-REST-ConsumingApis/src/main/java/company/controller/Consume_RestTemplate.java
+++ Spring-18-REST-ConsumingApis/src/main/java/company/controller/Consume_RestTemplate.java
@@ -3,6 +3,7 @@ package company.controller;
 import company.dto.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -28,6 +29,13 @@ public class Consume_RestTemplate {
         return responseEntity.getBody();
     }
 
+    @GetMapping("{id}")
+    public Object readUserById(@PathVariable("id") Long userId){
+
+        String URL = URI + "/{id}";
+
+        return restTemplate.getForObject(URL, Object.class, userId);
+    }
 
 
 }
