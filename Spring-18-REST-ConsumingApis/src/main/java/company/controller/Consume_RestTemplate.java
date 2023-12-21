02491163ab50bc5f1f1f0af36c2f760d97bbbc03
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2023-12-21 12:46:42 -0500

    Spring-18-REST-ConsumingApis  REST Template

--- Spring-18-REST-ConsumingApis/src/main/java/company/controller/Consume_RestTemplate.java
+++ Spring-18-REST-ConsumingApis/src/main/java/company/controller/Consume_RestTemplate.java
@@ -1,12 +1,15 @@
 package company.controller;
 
 import company.dto.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.awt.*;
+import java.util.Arrays;
 
 
 @RestController
@@ -14,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 public class Consume_RestTemplate {
 
     private final String URI = "https://jsonplaceholder.typicode.com/users";
+    private final String URI2 = "https://dummyapi.io/data/v1/user?limit=10";
     private final RestTemplate restTemplate;
 
     public Consume_RestTemplate(RestTemplate restTemplate) {
@@ -35,6 +39,21 @@ public class Consume_RestTemplate {
         String URL = URI + "/{id}";
 
         return restTemplate.getForObject(URL, Object.class, userId);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumeFromDummyApi(){
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","6298ebfecd0551211fce37a6");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange(URI2, HttpMethod.GET, entity,Object.class);
+
+        return response;
     }
 
 
