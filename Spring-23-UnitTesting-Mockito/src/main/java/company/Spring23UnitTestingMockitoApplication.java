9a40e0d3ddba1c148d24faf1d2cc252e05e90a95
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2024-01-17 17:47:53 -0500

    Spring-23-UnitTesting-Mockito

---
+++ Spring-23-UnitTesting-Mockito/src/main/java/company/Spring23UnitTestingMockitoApplication.java
@@ -0,0 +1,20 @@
+package company;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Spring23UnitTestingMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring23UnitTestingMockitoApplication.class, args);
+    }
+
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+}
---
+++ Spring-23-UnitTesting-Mockito/src/test/java/company/service/impl/UserServiceImplTest.java
@@ -0,0 +1,7 @@
+package company.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+}
