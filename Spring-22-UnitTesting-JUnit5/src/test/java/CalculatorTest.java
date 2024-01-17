24a16f304d9a188a59c7ebbe1b5caf297c6f9d5d
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2024-01-17 09:46:40 -0500

    Spring-22-UnitTesting-JUnit5

--- Spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ Spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -1,39 +1,67 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    @BeforeAll
+    static void setUpAll() {
+        System.out.println("BeforeAll is executed");
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        System.out.println("AfterAll is executed");
+    }
+
+
+    @BeforeEach
+    void setUpEach() {
+        System.out.println("BeforeEach is executed");
+    }
+
+
+    @AfterEach
+    void tearDownEach() {
+        System.out.println("AfterEach is executed");
+    }
+
     @Test
-    void testCase1(){
+    void testCase1() {
+        System.out.println("Test Case 1");
         fail("Not implemented yet");
     }
 
     @Test
-    void testCase2(){
+    void testCase2() {
+        System.out.println("Test Case 2");
 //        assertTrue(Calculator.operator.equals("add"));
         assertEquals("add", Calculator.operator);
     }
 
     @Test
-    void testCase3(){
+    void testCase3() {
+        System.out.println("Test Case 3");
         boolean result = Calculator.add(2, 2) == 4;
         assertTrue(result);
     }
 
     @Test
-    void testCase4(){
+    void testCase4() {
+        System.out.println("Test Case 4");
         boolean result = Calculator.add(2, 2) == 5;
         assertFalse(result);
     }
 
     @Test
-    void testCase5(){
-      assertArrayEquals(new int[]{1,2,3}, new int[]{1,2,3}, "Array equals failed");
+    void testCase5() {
+        System.out.println("Test Case 5");
+        assertArrayEquals(new int[]{1, 2, 3}, new int[]{1, 2, 3}, "Array equals failed");
     }
 
     @Test
-    void testCase6(){
+    void testCase6() {
+        System.out.println("Test Case 6");
         String nullString = null;
         assertNull(nullString);
 
@@ -43,7 +71,8 @@ class CalculatorTest {
 
 
     @Test
-    void testCase7(){
+    void testCase7() {
+        System.out.println("Test Case 7");
         Calculator c1 = new Calculator();
         Calculator c2 = c1;
         Calculator c3 = new Calculator();
@@ -54,19 +83,30 @@ class CalculatorTest {
 
 
     @Test
-    void testCase8(){
-
+    void testCase8() {
+        System.out.println("Test Case 8");
 
     }
 
 
     @Test
     void add() {
-        int actual = Calculator.add(2,3);
+        System.out.println("Test Case Add");
+
+        int actual = Calculator.add(2, 3);
         assertEquals(5, actual, "Addition test is failed");
 //        fail("Not implemented yet");
     }
 
+    @Test
+    void add2() {
+        System.out.println("Test Case Add 2");
+
+        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(5, 2));
+
+//        assertThrows(IllegalArgumentException.class, () -> Calculator.add2(2, 5));
+
+    }
 
 
 }
