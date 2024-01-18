220449e02126cb06a22e299db014d478cb2b6c0d
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2024-01-18 15:37:51 -0500

    Spring-23-UnitTesting-Mockito

---
+++ Spring-23-UnitTesting-Mockito/src/test/java/company/service/impl/ProjectServiceImplTest.java
@@ -0,0 +1,69 @@
+package company.service.impl;
+
+import company.dto.ProjectDTO;
+import company.entity.Project;
+import company.mapper.ProjectMapper;
+import company.repository.ProjectRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProjectServiceImplTest {
+
+    @Mock
+    ProjectRepository projectRepository;
+    @Mock
+    ProjectMapper projectMapper;
+
+    @InjectMocks
+    ProjectServiceImpl projectServiceImpl;
+
+    @Test
+    void getByProjectCode_test() {
+        Project project = new Project();
+        ProjectDTO projectDTO = new ProjectDTO();
+
+        when(projectRepository.findByProjectCode(anyString())).thenReturn(project);
+        when(projectMapper.convertToDto(project)).thenReturn(projectDTO);
+
+        ProjectDTO projectDTO1 = projectServiceImpl.getByProjectCode(anyString());
+
+        verify(projectRepository).findByProjectCode(anyString());
+        verify(projectMapper).convertToDto(any(Project.class));
+
+        assertNotNull(projectDTO1);
+    }
+
+
+    @Test
+    void getByProjectCode_exception_test() {
+        when(projectRepository.findByProjectCode("")).thenThrow(new RuntimeException("Project Not Found"));
+
+        Throwable exception = assertThrows(RuntimeException.class, () -> projectServiceImpl.getByProjectCode(""));
+
+        verify(projectRepository).findByProjectCode(anyString());
+
+        assertEquals("Project Not Found", exception.getMessage());
+    }
+
+    @Test
+    void save_test(){
+        ProjectDTO projectDTO = new ProjectDTO();
+        Project project = new Project();
+
+        when(projectMapper.convertToEntity(projectDTO)).thenReturn(project);
+        when(projectRepository.save(project)).thenReturn(project);
+
+        projectServiceImpl.save(projectDTO);
+
+        verify(projectRepository).save(project);
+    }
+
+
+}
--- Spring-23-UnitTesting-Mockito/src/test/java/company/service/impl/UserServiceImplTest.java
+++ Spring-23-UnitTesting-Mockito/src/test/java/company/service/impl/UserServiceImplTest.java
@@ -1,7 +1,43 @@
 package company.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import company.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserServiceImpl userServiceImpl;
+
+    @Test
+    void deleteByUsername_test(){
+        userServiceImpl.deleteByUserName("mikesmith@employee.com");
+
+//        verify(userRepository).deleteByUserName("mikesmith@employee.com");
+//        verify(userRepository, times(2)).deleteByUserName("mikesmith@employee.com");
+
+        verify(userRepository, atLeastOnce()).deleteByUserName("mikesmith@employee.com");
+
+//        verify(userRepository, atLeast(5)).deleteByUserName("mikesmith@employee.com");
+//        verify(userRepository, atMostOnce()).deleteByUserName("mikesmith@employee.com");
+//        verify(userRepository, atMost(5)).deleteByUserName("mikesmith@employee.com");
+
+//        InOrder order = inOrder(userRepository);
+//
+//        order.verify(userRepository).findAll();
+//        order.verify(userRepository).deleteByUserName("mikesmith@employee.com");
+
+    }
 
 }
