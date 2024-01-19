291c37a3f6771b73ac84a29f546b39f88c798e72
stefanmitkovic <64182081+stefanmitkovic@users.noreply.github.com>
2024-01-19 06:59:34 -0500

    Spring-24-UnitTesting-MockMvc

--- Spring-24-UnitTesting-MockMvc/src/test/java/company/controller/WelcomeControllerTest.java
+++ Spring-24-UnitTesting-MockMvc/src/test/java/company/controller/WelcomeControllerTest.java
@@ -1,7 +1,53 @@
 package company.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(WelcomeController.class)
 class WelcomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void welcome_test() throws Exception {
+//        call /welcome endpoint
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/welcome")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult mvcResult = mockMvc.perform(request).andReturn();
+
+//        verify status and content "welcome" is returned
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+        assertEquals("welcome", mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    void welcome2_test() throws Exception {
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/welcome")
+                .accept(MediaType.APPLICATION_JSON);
+
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Welcome"))
+                .andReturn();
+    }
+
 
 }
